d9a7d032173c27dae76af44df595a93c322c7e2f
unknown <ahmedomar1007@gmail.com>
2022-03-07 15:46:08 +0200

    install router dom & daisyui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </Router>
+      </>
     </>
   )
 }
